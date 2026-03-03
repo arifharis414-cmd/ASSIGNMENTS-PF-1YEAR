0bd2a14c9df661a45f4261f4b0a616a2c5890d49
arifharis414-cmd <arifharis414@gmail.com>
2026-03-04 02:02:56 +0500

    Create MatrixSum.java

---
+++ MatrixSum.java
@@ -0,0 +1,40 @@
+//Haris Arif, #21-Sec B,First year Evening
+//Assignment Question: Create a java program which takes input of matrices and print the sum of its rows and coulmns, sums columns and also the sum of forward and reverse diagonals  
+
+import java.util.Scanner;
+
+public class MatrixSum {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int columns = input.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Element [" + i + "][" + j + "] = ");
+                matrix[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("\nMatrix you entered is:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+     
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
